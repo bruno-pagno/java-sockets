8e0e4b5f0920c0f68879d62f012b8e18dc7b8db3
Bruno Pagno <brunopagno@usp.br>
2022-07-30 10:23:51 -0300

    feat: added protocol logic

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,25 @@
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("localhost", 5555);
+        System.out.println("Connection stablished. You can now send messages");
+        Scanner scanner = new Scanner(System.in);
+
+        String userInput = null;
         DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-        output.writeUTF("hello world");
+
+        do {
+            System.out.print("> ");
+            userInput = scanner.nextLine();
+            output.writeUTF(userInput);
+        } while (!userInput.equals("bye"));
+
+        System.out.println("end of communication");
+        output.close();
+        socket.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,5 @@
 import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -16,13 +17,27 @@ public class Server {
 
     private void listenForMessages(Socket socket) throws IOException {
         DataInputStream input = new DataInputStream(socket.getInputStream());
-        String receivedMessage = input.readUTF();
-        System.out.println("Received message: " + receivedMessage);
+
+        String receivedMessage = null;
+        do {
+            try {
+                receivedMessage = input.readUTF();
+                System.out.println("Received message: " + receivedMessage);
+            } catch (EOFException eof) {
+                System.out.println("Error: client disconnected");
+                System.exit(1);
+            }
+        } while (!receivedMessage.equals("bye"));
+        input.close();
     }
 
     public static void main(String[] args) throws IOException {
         Server server = new Server(5555);
+        System.out.println("Server is up. Waiting for connection");
         Socket socket = server.acceptConnections();
+        System.out.println("Connection stablished. Waiting for messages");
         server.listenForMessages(socket);
+        System.out.println("end of communication");
+        socket.close();
     }
 }
